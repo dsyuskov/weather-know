324a83a2a6fcc0e94d5f947aaf4578a527431610
int_21h <yuskovss@gmail.com>
2020-02-29 11:02:11 +0400

    Updates

---
+++ precache-manifest.906bf67d3f596741b29ca91fa25803f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c08a6da08bcd1db2b50a81d0a5dc02f8",
+    "url": "/weather-know/index.html"
+  },
+  {
+    "revision": "19c18d56900d8736539b",
+    "url": "/weather-know/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "bf15a8c40cc6490b7dc1",
+    "url": "/weather-know/static/js/2.585c6af8.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/weather-know/static/js/2.585c6af8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19c18d56900d8736539b",
+    "url": "/weather-know/static/js/main.9b9bd318.chunk.js"
+  },
+  {
+    "revision": "b06b76352f8ade0136bc",
+    "url": "/weather-know/static/js/runtime-main.dceed6b2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/weather-know/static/media/logo.5d5d9eef.svg"
+  }
+]);
